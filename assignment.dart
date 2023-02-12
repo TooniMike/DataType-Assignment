611b50910f76d0c8a90064a0813b2707bacca875
TooniMike <113641377+TooniMike@users.noreply.github.com>
2023-02-12 17:57:58 +0100

    first commit

---
+++ assignment.dart
@@ -0,0 +1,14 @@
+// Below is an int datatype
+int amount = 6000;
+
+// Below is a double datatype
+double size = 16.5;
+
+// Below is a String datatype
+String company = 'Power Leanrning Program';
+
+// Below is a List datatype
+List ages = [23, 1, 66, 77, 13];
+
+// Below is a map datatype
+var studentResults = {'John': 23, 'James': 5, 'Joe': 55, 'Peter': 23};
